19f2e14a98d3429e78551aaa9442798ad274f3d3
RG8452 <ryang8452@yahoo.com>
2018-02-12 22:46:20 -0600

    Added in methodology for creating grid and drawing on basic board elements

--- src/org/Othello.java
+++ src/org/Othello.java
@@ -17,12 +17,13 @@ public class Othello
 	public static JFrame jf; //Frame on which the game is located
 	public static OthelloPanel op; //Panel which handles drawing and input
 	public static JLabel jl; //Label to display current turn
+	public static char[][] grid = new char[8][8]; //Create grid in which to store pieces
 
 	public static void main(String[] args)
 	{
 		jf = new JFrame("Othello"); //Initialize components
 		op = new OthelloPanel();
-		jl = new JLabel("Current turn here");
+		jl = new JLabel("Current player: Black");
 
 		op.setPreferredSize(new Dimension(800, 800)); //Set sizes
 		jl.setSize(800, 20);
@@ -34,5 +35,9 @@ public class Othello
 		jf.setFocusTraversalKeysEnabled(true); //Allow key traversal
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set up close operation
 		jf.setVisible(true); //Set visible
+		grid[3][3] = 'W';
+		grid[3][4] = 'B';
+		grid[4][3] = 'B';
+		grid[4][4] = 'W';
 	}
 }
--- src/org/OthelloPanel.java
+++ src/org/OthelloPanel.java
@@ -4,6 +4,10 @@ package org;
  * It will handle all inputs and drawing
  */
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -13,6 +17,9 @@ import javax.swing.JPanel;
 
 public class OthelloPanel extends JPanel
 {
+	private static Color background = new Color(0, 255, 128);	//Color of the background
+	private static int offset = 5;	//Variable for the offset of the pieces from the corner
+	
 	//Default constructor
 	public OthelloPanel()
 	{
@@ -20,11 +27,62 @@ public class OthelloPanel extends JPanel
 		addKeyListener(oHandler); //Add handlers
 		addMouseListener(oHandler);
 		setFocusable(true); //Allow focus
+		setBackground(background);
+	}
+	
+	//Override painting method for unique drawing
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);		//Call super to paint on frame
+		Graphics2D g2d = (Graphics2D)g;	//Establish better graphics system
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);	//Enable antialiasing
+		
+		drawGrid(g2d);
+		drawPieces(g2d);
+	}
+	
+	//Draws in circles for the pieces depending on what the grid contains
+	private void drawPieces(Graphics2D g2d)
+	{
+		for(int row=0; row < 8; row++)
+		{
+			for(int col=0; col < 8; col++)	//Loop through every piece
+			{
+				switch(Othello.grid[row][col])	//Switch the current char
+				{
+					case '\0':		//Dummy case / null
+						continue;	//Ignore and go to the next slot
+					case 'W':		//White
+						g2d.setColor(Color.white);
+						break;
+					case 'B':		//Black
+						g2d.setColor(Color.black);
+						break;
+					case 'R':		//Red/faded/whatever we decide
+						g2d.setColor(Color.red);
+						break;
+				}
+				g2d.fillOval(100*col + offset, 100*row + offset, 100 - (offset << 1), 100 - (offset << 1));	//Draw the circle
+			}
+		}
+	}
+	
+	//Draws in the grid to establish that checkerboard pattern
+	private void drawGrid(Graphics2D g2d)
+	{
+		g2d.setColor(Color.black);
+		for(int i=0; i<8; i++)
+		{
+			g2d.drawLine(0, 100*i, 800, 100*i);
+			g2d.drawLine(100*i, 0, 100*i, 800);
+		}
 	}
 
 	/*
 	 * This private class does all the input handling for the Othello Handling 
 	 * It will also likely control repaint rate
+	 * KeyListening may prove to be pointless
 	 */
 	private class OthelloHandler implements KeyListener, MouseListener
 	{
@@ -43,6 +101,7 @@ public class OthelloPanel extends JPanel
 		public void mouseClicked(MouseEvent e)
 		{
 			System.out.println(String.format("Clicked at %d, %d", e.getX(), e.getY()));
+			System.out.println("Click coords - Row: " + (int)(e.getY() / 100) + "\tCol: " + (int)(e.getX() / 100));
 		}
 
 		
